20d090698069d8904fc7fda33fd1fb73b34003c9
spike014 <l1ng14@foxmail.com>
2023-04-08 14:00:26 +0800

    refactor(chapter): fix didn't add sub chapter

--- book/chapter.py
+++ book/chapter.py
@@ -3,31 +3,16 @@ import markdown2
 from ebooklib import epub
 
 
-def convert_chapter(root, title, html_file_name, context):
-    with open(os.path.join(root, context['file']), 'r') as file:
-        chapter = epub.EpubHtml(title=title, file_name=html_file_name, lang="zh")
-        chapter.content = markdown2.markdown(file.read(), extras=["fenced-code-blocks", "latex"])
-        return chapter
-
 def add_items(book, content, options):
-    for title, context in content.items(): 
-        paths = context['file'].split('.')
-        if len(paths) < 2:
-            print(f"Error: chapter file name {context['file']} invalid.")
-            exit(1)
-        html_file_name = paths[len(paths)-2] + ".xhtml"
-        chapter = convert_chapter(options.project, title, html_file_name, context)
+    for title, context in content.items():
+        html_file_name = get_html_file_name(context['file'])
+        chapter = get_chapter(context, options, title)
         book.add_item(chapter)
         child_items = []
         
         for child_title, child_context in context['children'].items(): 
-            child_paths = child_context['file'].split('.')
-            if len(child_paths) < 2:
-                print(f"Error: chapter file name {child_context['file']} invalid.")
-                exit(1)
-            child_html_file_name = f"{child_paths[len(child_paths)-2]}.xhtml"
-            child_item = convert_chapter(options.project, child_title, child_html_file_name, child_context)
-
+            child_item = get_chapter(child_context, options, child_title)
+            book.add_item(child_item)
             book.spine.append(child_item)
             child_items.append(child_item)
 
@@ -38,3 +23,24 @@ def add_items(book, content, options):
                 child_items
               )
         )
+
+
+def get_chapter(context, options, title):
+    html_file_name = get_html_file_name(context['file'])
+    chapter = convert_chapter(options.project, title, html_file_name, context)
+    return chapter
+
+
+def get_html_file_name(filename):
+    paths = filename.split('.')
+    if len(paths) < 2:
+        print(f"Error: chapter file name {filename} invalid.")
+        exit(1)
+    return f"{paths[len(paths)-2]}.xhtml"
+
+
+def convert_chapter(root, title, html_file_name, context):
+    with open(os.path.join(root, context['file']), 'r') as file:
+        chapter = epub.EpubHtml(title=title, file_name=html_file_name, lang="zh")
+        chapter.content = markdown2.markdown(file.read(), extras=["fenced-code-blocks"])
+        return chapter
